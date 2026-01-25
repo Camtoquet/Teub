24a341c0a4bf3459154002cb2228e96017ffa6db
Camtoquet <camille.maffre.lmc@outlook.fr>
2026-01-25 01:24:01 +0100

    tronc du quizz

---
+++ quizz/quizz.py
@@ -0,0 +1,19 @@
+
+Q_and_R = {}
+
+
+def question(question):
+    '''
+    Will ask a question with the format attribuate
+    such as several awnser or close/far etc.
+    '''
+
+
+
+def quizz(nb_q):
+    '''
+    Will ask a number of nb_q questions for making the quizz, 
+    counting the number of good questions over the total as well
+    '''
+    for i in range (0,nb_q):
+        break
